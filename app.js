bafc9835f2e63897841c8cf0b5398b0e58673f6c
Pradeep Khatri <pradeepkhatri121@gmail.com>
2023-07-31 20:21:06 +0530

    update mern

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27117/todolistDB");
+mongoose.connect("mongodb://127.0.0.1/todolistDB");
 
 const itemsSchema = new mongoose.Schema({
     name : String
@@ -24,33 +25,67 @@ const item3   = new Item ({name:" hit <- this to delete"});
 
 const defaultItems = [item1 , item2 , item3];
 
-Item.insertMany(defaultItems , function(err) {
-    if(err){
-        console.log(err);
-    }else{
-        console.log("Successfully saved default items !!");
-    }
+
+
+app.get("/", function (req, res) {
+
+    Item.find({} , function(err,founditems){
+
+        if(founditems.length === 0 ){
+            Item.insertMany(defaultItems , {function(err) {
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("Successfully saved default items !!");
+                }
+            }});
+            
+            res.redirect("/");
+        }else{
+            res.render('list', { KindOfDay: "Today", newlistitems:founditems });   
+            // console.log(founditems.name);    
+        }
+    })
+
 });
 
-
-app.get("/", function(req,res) {
-
-    res.render('list' , {KindOfDay: "Today", newlistitems : items});
-});
-
-app.get("/added",(req,res) => {
-    res.render('list', {KindOfDay: day, newlistitems: items});
+app.get("/added", (req, res) => {
+    res.render('list', { KindOfDay: day, newlistitems: items });
 })
 
-app.post("/" , function(req , res) {
-    var item = req.body.newItem;
+app.post("/", function (req, res) {
+    const itemName = req.body.newItem;
+
+    const item = new Item({
+        name : itemName
+    });
+
+    item.save();
+    res.redirect("/");
+
+
+});
+
+app.post("/delete" , function(req, res) {
     
-    items.push(item);
+    const {check} = req.body;
 
-    res.redirect("/added");
-})
+
+
+    Item.findByIdAndRemove(check , function(err){
+        if(err){
+            console.log("err");
+        }else{
+            console.log("successsfully deleted..");
+            res.redirect("/");
+
+        }
+    });
+}); 
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Port started at port 3000");
 });
+
+
 
